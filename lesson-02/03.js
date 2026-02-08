5abb0a6526a74685a4b3f04e0c54c66faaca0126
Arina Khakymova <arina.khakymova@gmail.com>
2026-02-08 16:33:40 -0300

    Complete lesson-02/02.js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,16 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
-let grade
+let grade;
 // your code
+if (score >= 90) {
+    grade = 'A'
+} else if (score >=80) {
+    grade = 'B'
+} else if (score >=70) {
+    grade = 'C'
+} else if (score >=50) {
+    grade = 'D'
+} else {
+    grade = 'F'
+}
