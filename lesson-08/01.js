31f21c48bb68cc00e5fc464c2d5c11a40546b1c5
Arina Khakymova <arina.khakymova@gmail.com>
2026-03-29 11:29:25 -0300

    Fix lesson-08/01.js

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,15 +6,29 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
+// function capitalizeWords(string) {
+//   let newString = "";
+//   for (let i = 0; i < string.length; i++) {
+//     if (i === 0 || string[i - 1] === " ") {
+//       newString += string[i].toUpperCase();
+//     } else {
+//       newString += string[i];
+//     }
+//   }
+//   return newString;
+// }
+
 function capitalizeWords(string) {
+  if (!string) return "";
+
   let newString = "";
   for (let i = 0; i < string.length; i++) {
     if (i === 0 || string[i - 1] === " ") {
-      newString += string[i].toUpperCase();
+      newString += string[i] ? string[i].toUpperCase() : "";
     } else {
       newString += string[i];
     }
   }
-  return newString
+  return newString;
 }
 
